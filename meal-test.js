30c0bb41777d7af3f35ae7375b4828c527ec7915
Amy Holt <amy.elizabeth.holt@gmail.com>
2019-08-24 12:31:40 -0600

    Update spacing in meal test

--- meal-test.js
+++ meal-test.js
@@ -5,6 +5,7 @@ describe('Meal', function() {
 
   it.skip('should have a name, special requests, and table number', function() {
     var shrimpTacos = new Meal('Shrimp Tacos', ["no cheese"], 14);
+    
     assert.equal(shrimpTacos.name, 'Shrimp Tacos');
     assert.equal(shrimpTacos.specialRequests, ["no cheese"]);
     assert.equal(shrimpTacos.tableNumber, 14);
@@ -12,22 +13,26 @@ describe('Meal', function() {
 
   it.skip('should be able to have a name, special requests, and table number', function() {
     var appetizer = new Meal('Jicama & Cucumber', [], 14);
+    
     assert.equal(appetizer.name, 'Jicama & Cucumber');
     assert.equal(appetizer.specialRequests, []);
   });
 
   it.skip('should default to not complete', function() {
     var appetizer = new Meal('Jicama & Cucumber', [], 14);
+    
     assert.equal(customer44.complete, false);
   });
 
   it.skip('should have a table number', function() {
     var shrimpTacos = new Meal('Shrimp Tacos', ["no cheese"], 14);
+    
     assert.equal(shrimpTacos.tableNumber, 14);
   });
 
   it.skip('should have be able to have a different table number', function() {
     var appetizer = new Meal('Jicama & Cucumber', [], 14);
+    
     assert.equal(appetizer.tableNumber, 14);
   });
 
