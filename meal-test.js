e045640ff03c9999a90e251d86b67b31772a4c7b
Amy Holt <amy.elizabeth.holt@gmail.com>
2019-10-29 12:11:18 -0600

    Update meal-test.js

--- meal-test.js
+++ meal-test.js
@@ -11,11 +11,12 @@ describe('Meal', function() {
     assert.equal(shrimpTacos.tableNumber, 14);
   });
 
-  it.skip('should be able to have a name, special requests, and table number', function() {
-    var appetizer = new Meal('Jicama & Cucumber', [], 14);
+  it.skip('should be able to have a diff. name, special requests, and table number', function() {
+    var appetizer = new Meal('Jicama & Cucumber', [], 12);
     
     assert.equal(appetizer.name, 'Jicama & Cucumber');
     assert.deepEqual(appetizer.specialRequests, []);
+    assert.equal(appetizer.tableNumber, 12);
   });
 
   it.skip('should default to not complete', function() {
@@ -24,19 +25,7 @@ describe('Meal', function() {
     assert.equal(appetizer.complete, false);
   });
 
-  it.skip('should have a table number', function() {
-    var shrimpTacos = new Meal('Shrimp Tacos', ["no cheese"], 14);
-    
-    assert.equal(shrimpTacos.tableNumber, 14);
-  });
-
-  it.skip('should have be able to have a different table number', function() {
-    var appetizer = new Meal('Jicama & Cucumber', [], 14);
-    
-    assert.equal(appetizer.tableNumber, 14);
-  });
-
-  it.skip('should be able to pay for a rental car', function() {
+  it.skip('should be able to be completed', function() {
     var appetizer = new Meal('Jicama & Cucumber', [], 14);
     var completeMeal = appetizer.getMade();
 
