fbfc0262204228a4976ae76cc66b0d58bd5118ef
Chris Spohn <chris.j.spohn@gmail.com>
2020-11-07 12:14:27 -0700

    Complete player-test.js

--- player-test.js
+++ player-test.js
@@ -3,34 +3,42 @@ var Player = require('./player');
 
 describe('Player', function() {
 
-  it('should have a name, special requests, and table number', function() {
-    var shrimpTacos = new Meal('Shrimp Tacos', ["no cheese"], 14);
+  it('should have a name, jersey number, and injuries', function() {
+    var player = new Player('Jeff', 27, ['bruised rib']);
 
-    assert.equal(shrimpTacos.name, 'Shrimp Tacos');
-    assert.deepEqual(shrimpTacos.specialRequests, ["no cheese"]);
-    assert.equal(shrimpTacos.tableNumber, 14);
+    assert.equal(player.name, 'Jeff');
+    assert.equal(player.number, 27);
+    assert.deepEqual(shrimpTacos.injuries ['bruised rib']);
   });
 
-  it('should be able to have a diff. name, special requests, and table number', function() {
-    var appetizer = new Meal('Jicama & Cucumber', [], 12);
+  it.skip('should be able to have a diff. name, jersey number, and injuries', function() {
+    var player = new Player('Mary', 22, ['sprained ankle']);
 
-    assert.equal(appetizer.name, 'Jicama & Cucumber');
-    assert.deepEqual(appetizer.specialRequests, []);
-    assert.equal(appetizer.tableNumber, 12);
+    assert.equal(player.name, 'Mary');
+    assert.equal(player.number, 22);
+    assert.deepEqual(player.injuries, ['sprained ankle']);
   });
 
-  it('should default to not complete', function() {
-    var appetizer = new Meal('Jicama & Cucumber', [], 14);
+  it.skip('should default to not playing', function() {
+    var player = new Player('Mark', 44, []);
 
-    assert.equal(appetizer.complete, false);
+    assert.equal(player.playing, false);
   });
 
-  it('should be able to be completed', function() {
-    var appetizer = new Meal('Jicama & Cucumber', [], 14);
-    var completeMeal = appetizer.getMade();
+  it.skip('should be able to be playing', function() {
+    var player = new Player('Mark', 44, []);
+    var playingPlayer = player.putMeInCoach();
 
-    assert.equal(appetizer.complete, true);
-    assert.equal(completeMeal, 'Order up - Jicama & Cucumber');
+    assert.equal(player.playing, true);
+    assert.equal(playingPlayer, `I'm ready for action!`);
   });
+
+  it.skip('should not be able to be playing if it has an injury', function() {
+    var player = new Player('Randall', 23, ['stubbed toe']);
+    var playingPlayer = player.putMeInCoach();
+
+    assert.equal(player.playing, false);
+    assert.equal(playingPlayer, `I can't go in, I'm all borked up.`)
+  })
 
 });
--- player.js
+++ player.js
@@ -1,5 +1,6 @@
 class Player {
 
+
 }
 
 module.exports = Player;
